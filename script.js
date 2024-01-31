d6eace5ba729614c6d84d5ad813a49e1d4cc6103
Kamran Zafar <kamranzafar4343@gmail.com>
2024-02-01 02:21:29 +0500

    done with js

--- script.js
+++ script.js
@@ -9,3 +9,53 @@ const showMore = document.getElementById("more-button")
 let inputData=""
 let page=1;
 
+async function searchImages(){
+    
+    inputData= inputEl.value;
+    const url ='https://api.unsplash.com/search/photos?page=${page}&query={inputData}&client_id=${accessKey}'
+    
+    const response = await fetch(url)
+    const data= await  response.json() 
+
+
+    const results = data.results
+
+    if(page===1){
+        searchResults.innerHTML="";
+    }
+
+    results.map((results) =>{
+        const imgWrapper =document.createElement('div');
+        imageWrapper.classlist.add('search-result');
+        const image =  document.createElement('img');
+        image.src = results.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href =result.links.innerHTML;
+        imageLink.target="_blank";
+        imageLink.textContent= result.alt_description;
+      
+        image.Wrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.append(imageWrapper);
+    
+    
+    });
+
+    page++
+    if(page > 1){
+        showMore.style.display="block"
+    }
+}
+
+
+formEl.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    page=1;
+    searchImages()
+})
+
+showMore.addEventListener("click", (event)=>{
+    
+    searchImages()
+})
