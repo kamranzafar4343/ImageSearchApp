0aa591e34cff9eeb15e1955c3cc592bf36205844
Kamran Zafar <kamranzafar4343@gmail.com>
2024-02-01 01:52:55 +0500

    added js

---
+++ script.js
@@ -0,0 +1,11 @@
+const accessKey = "ZBLKfjQVWSjuj0ctrmN_w8Ety1U6QvJGuRQhV2qbE50"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector("search-results")
+const showMore = document.getElementById("more-button")
+
+
+let inputData=""
+let page=1;
+
