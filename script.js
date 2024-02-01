2bc87804aa6a30db692a0fd34262bba059164301
Kamran Zafar <kamranzafar4343@gmail.com>
2024-02-01 16:31:17 +0500

    fix - identified syntax errors

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-const accessKey = "ZBLKfjQVWSjuj0ctrmN_w8Ety1U6QvJGuRQhV2qbE50"
+const accessKey = "ZBLKfjQVWSjuj0ctrmN_w8Ety1U6QvJGuRQhV2qbE50";
 
-const formEl = document.querySelector("form")
-const inputEl = document.getElementById("search-input")
-const searchResults = document.querySelector("search-results")
-const showMore = document.getElementById("more-button")
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("more-button");
 
 
-let inputData=""
-let page=1;
+let inputData= "";
+let page= 1;
 
-async function searchImages(){
+async function searchImages() {
     
     inputData= inputEl.value;
-    const url ='https://api.unsplash.com/search/photos?page=${page}&query={inputData}&client_id=${accessKey}'
+    const url ='https://api.unsplash.com/search/photos?page=${page}&query={inputData}&client_id=${accessKey}';
     
-    const response = await fetch(url)
-    const data= await  response.json() 
+    const response = await fetch(url);
+    const data= await  response.json();
 
 
-    const results = data.results
+    const results = data.results;
 
-    if(page===1){
-        searchResults.innerHTML="";
+    if(page === 1){
+        searchResults.innerHTML= "";
     }
 
-    results.map((results) =>{
-        const imgWrapper =document.createElement('div');
-        imageWrapper.classlist.add('search-result');
-        const image =  document.createElement('img');
+    results.map((result) =>{
+        const imageWrapper =document.createElement("div");
+        imageWrapper.classlist.add("search-result");
+        const image =  document.createElement("img");
         image.src = results.urls.small;
         image.alt = result.alt_description;
-        const imageLink = document.createElement('a');
-        imageLink.href =result.links.innerHTML;
-        imageLink.target="_blank";
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target= "_blank";
         imageLink.textContent= result.alt_description;
       
-        image.Wrapper.appendChild(image);
+        imageWrapper.appendChild(image);
         imageWrapper.appendChild(imageLink);
-        searchResults.append(imageWrapper);
+        searchResults.appendChild(imageWrapper);
     
     
     });
 
-    page++
+    page++;
     if(page > 1){
-        showMore.style.display="block"
+        showMore.style.display= "block";
     }
 }
 
 
 formEl.addEventListener("submit", (event)=>{
-    event.preventDefault()
-    page=1;
-    searchImages()
+    event.preventDefault();
+    page = 1;
+    searchImages();
 })
 
-showMore.addEventListener("click", (event)=>{
-    
-    searchImages()
+showMore.addEventListener("click", ()=>{
+    searchImages();
 })
