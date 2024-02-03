3b7b1fa0daf59c878b29c278fd69020a408c37aa
Kamran Zafar <kamranzafar4343@gmail.com>
2024-02-03 11:21:59 +0500

    little bug fixes - chatgpt

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let page= 1;
 async function searchImages() {
     
     inputData= inputEl.value;
-    const url ='https://api.unsplash.com/search/photos?page=${page}&query={inputData}&client_id=${accessKey}';
+    const url =`https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
     
     const response = await fetch(url);
     const data= await  response.json();
@@ -26,9 +26,9 @@ async function searchImages() {
 
     results.map((result) =>{
         const imageWrapper =document.createElement("div");
-        imageWrapper.classlist.add("search-result");
+        imageWrapper.classList.add("search-result");
         const image =  document.createElement("img");
-        image.src = results.urls.small;
+        image.src = result.urls.small;
         image.alt = result.alt_description;
         const imageLink = document.createElement("a");
         imageLink.href = result.links.html;
